b684ed4b7c12d453d6f7dfff09ed91ee9f121919
dpw1 <diego.boarutto.fortes@gmail.com>
2021-05-29 08:26:12 -0300

    [ADD] sass

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import Products from "./Products";
+
 function App() {
   const [storeData, setStoreData] = useState(null);
   const [products, setProducts] = useState([]);
 
   const populateProducts = async () => {
-    const url = `https://app.ecwid.com/api/v3/${storeData.store_id}/products?token=${storeData.access_token}`;
+    // const url = `https://app.ecwid.com/api/v3/${storeData.store_id}/products?token=${storeData.access_token}`;
+    const url = `https://app.ecwid.com/api/v3/37374877/products?token=secret_EkQGe8SC4V4DHgSrnkmXsQsneDQ9JwQ4`;
 
     const { data: _products } = await axios.get(url);
     setProducts(_products);
@@ -18,8 +21,6 @@ function App() {
   }, []);
 
   useEffect(() => {
-    setStoreData(window.EcwidApp.getPayload());
-
     (async () => {
       populateProducts();
     })();
@@ -29,7 +30,7 @@ function App() {
 
   return (
     <div className="App">
-      this is react: {products && JSON.stringify(products)}
+      <Products products={products}></Products>
     </div>
   );
 }
---
+++ src/Products.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Products.scss";
+
+export default function Products(props) {
+  const { products } = props;
+  return (
+    <div className="Products  named-area">
+      <div className="named-area__body">
+        <div className="a-card a-card--compact">
+          <div className="a-card__paddings">
+            <h4>Products</h4>
+
+            {products &&
+              products.hasOwnProperty("items") &&
+              products.items.map((e) => {
+                return (
+                  <div
+                    key={e.id}
+                    className="list-element list-element--compact list-element--has-hover list-element--inline-mode">
+                    <div className="list-element__image">
+                      <img src={e.thumbnailUrl} alt={e.name} />
+                    </div>
+                    <div className="list-element__content">
+                      <div className="list-element__info">
+                        <div className="list-element__header">
+                          <div className="list-element__main-info">
+                            <div className="list-element__title">
+                              <span>{e.name}</span>
+                            </div>
+                            <div className="list-element__description">
+                              <span className="muted">0001</span>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="list-element__status-row">
+                          <span className="order__date-wrapper">
+                            Upsell Products
+                          </span>
+                          <div className="list-element__data-row">
+                            <span className="spacing--mr1">Cool product 1</span>
+                            <span className="spacing--mr1">Nice Product 2</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
