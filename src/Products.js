10fbd7c7ec0f49ded0e72769b7e1004f2bf2db2a
dpw1 <diego.boarutto.fortes@gmail.com>
2023-07-31 16:11:37 +0100

    PUBLISH UPDATES

--- src/Products.js
+++ src/Products.js
@@ -35,7 +35,7 @@ export default function Products({ upsellProducts }) {
         <div className="a-card a-card--compact">
           <div className="a-card__paddings">
             <h1 className="Products-title settings-page__title spacing--mt2">
-              Products!!!!!
+              Products!
             </h1>
 
             {products &&
@@ -46,8 +46,7 @@ export default function Products({ upsellProducts }) {
                   key={e.id}
                   product={e}
                   upsell={
-                    upsellProducts.filter((x) => x.id === e.id).bundle.length >=
-                    1
+                    upsellProducts.filter((x) => x.id === e.id).length >= 1
                       ? upsellProducts.filter((x) => x.id === e.id)[0].bundle
                       : []
                   }></ProductItem>
