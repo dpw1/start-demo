70ce8d33eb418287ae6b50c7dfdbc6d11470d439
dpw1 <diego.boarutto.fortes@gmail.com>
2023-08-06 22:35:19 +0100

    Updates

--- inject_js.js
+++ inject_js.js
@@ -4,50 +4,6 @@ window.ezfyEasyUpsellApp = (function () {
   const USE_CUSTOM_URLS =
     window.location.hostname === "ezfycode.com" ? true : false;
 
-  function _loadScript(src) {
-    return new Promise(function (resolve, reject) {
-      var s;
-      s = document.createElement("script");
-      s.src = src;
-      s.onload = resolve;
-      s.onerror = reject;
-      document.head.appendChild(s);
-    });
-  }
-
-  function _extractTextBetween(text, start, end) {
-    if (!start || !end) {
-      throw new Error(`Please add a "start" and "end" parameter`);
-    }
-
-    return text.split(start)[1].split(end)[0];
-  }
-
-  function _loadStyle(src) {
-    return new Promise(function (resolve, reject) {
-      let link = document.createElement("link");
-      link.href = src;
-      link.rel = "stylesheet";
-
-      link.onload = () => resolve(link);
-      link.onerror = () => reject(new Error(`Style load error for ${src}`));
-
-      document.head.append(link);
-    });
-  }
-
-  function _moveDOMElement(parent, child) {
-    document.querySelector(parent).appendChild(document.querySelector(child));
-  }
-
-  function _isProductPage() {
-    return /product/.test(window.location.href);
-  }
-
-  function _isCartPage() {
-    return /cart\/?$/.test(window.location.href.split("?")[0]);
-  }
-
   function _waitForElement(selector, delay = 50, tries = 100) {
     const element = document.querySelector(selector);
 
@@ -278,7 +234,11 @@ window.ezfyEasyUpsellApp = (function () {
     if (!$popup) {
       const html = `<div class="EzfyPopup">
       <div class="EzfyPopup-container">
+      <button onclick="handlePopupClose(event)" class="EzfyPopup-close"><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" class="icon icon-close" fill="none" viewBox="0 0 18 17"><path d="M.865 15.978a.5.5 0 00.707.707l7.433-7.431 7.579 7.282a.501.501 0 00.846-.37.5.5 0 00-.153-.351L9.712 8.546l7.417-7.416a.5.5 0 10-.707-.708L8.991 7.853 1.413.573a.5.5 0 10-.693.72l7.563 7.268-7.418 7.417z" fill="currentColor"></path></svg></button>
+      <div class="EzfyPopup-body">
       <p>hello</p>
+      </div>
+ 
     </div>
     </div>`;
 
@@ -286,6 +246,10 @@ window.ezfyEasyUpsellApp = (function () {
 
       $body.insertAdjacentHTML("beforeend", html);
     }
+  }
+
+  async function handlePopupClose(e) {
+    alert(e);
   }
 
   async function handlePopup() {}
@@ -367,6 +331,7 @@ window.ezfyEasyUpsellApp = (function () {
   }
 
   return {
+    handlePopupClose: handlePopupClose,
     init: function () {
       injectReacPlaceholder();
       start();
