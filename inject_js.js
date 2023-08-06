1366c9b72bf85aef1ced1980e0db0fc4a8dc64af
dpw1 <diego.boarutto.fortes@gmail.com>
2023-08-06 22:40:06 +0100

    Updates

--- inject_js.js
+++ inject_js.js
@@ -234,7 +234,7 @@ window.ezfyEasyUpsellApp = (function () {
     if (!$popup) {
       const html = `<div class="EzfyPopup">
       <div class="EzfyPopup-container">
-      <button onclick="handlePopupClose(event)" class="EzfyPopup-close"><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" class="icon icon-close" fill="none" viewBox="0 0 18 17"><path d="M.865 15.978a.5.5 0 00.707.707l7.433-7.431 7.579 7.282a.501.501 0 00.846-.37.5.5 0 00-.153-.351L9.712 8.546l7.417-7.416a.5.5 0 10-.707-.708L8.991 7.853 1.413.573a.5.5 0 10-.693.72l7.563 7.268-7.418 7.417z" fill="currentColor"></path></svg></button>
+      <button onclick="window.handlePopupClose(event)" class="EzfyPopup-close"><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" class="icon icon-close" fill="none" viewBox="0 0 18 17"><path d="M.865 15.978a.5.5 0 00.707.707l7.433-7.431 7.579 7.282a.501.501 0 00.846-.37.5.5 0 00-.153-.351L9.712 8.546l7.417-7.416a.5.5 0 10-.707-.708L8.991 7.853 1.413.573a.5.5 0 10-.693.72l7.563 7.268-7.418 7.417z" fill="currentColor"></path></svg></button>
       <div class="EzfyPopup-body">
       <p>hello</p>
       </div>
